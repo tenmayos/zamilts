e0bbaec00442cf6be977d887488f5f7bea4e4797
bassam <tenmayos_2@hotmail.com>
2023-09-17 12:34:26 +0300

    started implementing routing

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
