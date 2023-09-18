3b3f47b8f5f6c375bf6a234687e70992bbc146c3
bassam <tenmayos_2@hotmail.com>
2023-09-18 15:42:47 +0300

    removed starter basic css

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,8 +1,9 @@
+import '../styles/main.css'
 
 export default function Home() {
     return (
         <>
-        <h1>HOME PAGE</h1>
+        <h1 className="text-3xl font-bold underline bg-red-500">HOME PAGE</h1>
         </>
     )
 }
