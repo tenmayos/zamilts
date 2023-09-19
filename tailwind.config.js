6505d1e6127dd29f63e58de3de3edf7314bfc064
bassam <tenmayos_2@hotmail.com>
2023-09-19 10:47:24 +0300

    changed tailwind config content locations to more precise ones

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./src/*.jsx",
+    "./src/components/*.jsx",
+    "./src/pages/*.jsx",
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
