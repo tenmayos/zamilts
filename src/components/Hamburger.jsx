5f9906ee884a48aeb6f81cb9bd0385704e1d93c6
tenmayos <tenmayos_2@hotmail.com>
2023-09-22 21:48:51 +0300

    added hamburger component

---
+++ src/components/Hamburger.jsx
@@ -0,0 +1,12 @@
+function Hamburger(props) {
+
+    return (
+        <button
+            id='mobile-open-btn'
+            className={props.className}>
+            &#9776;
+        </button>
+    )
+}
+
+export default Hamburger;
