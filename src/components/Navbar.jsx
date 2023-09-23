2a8bd839a97961a9dba92a0983bf630a6d1636be
tenmayos <tenmayos_2@hotmail.com>
2023-09-23 08:18:00 +0300

    gave the links list its own component to reduce html in Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,44 +1,29 @@
-import logo from "../assets/png/logo.png";
 import Hamburger from "./Hamburger.jsx";
+import LinksList from './LinksList.jsx';
+import logo from "../assets/png/logo.png";
+
 
 function NavBar() {
-  const liStyle = "transition-all hover:text-green-400 p-1";
+  const ulStyles = 'flex space-x-4 text-center navbarAllignment text-white';
+  const liStyles = "transition-all hover:text-green-400 p-1";
 
   // will need a state to know when the hamburger has been clicked.
   return (
     <header className="flex border-black bg-gradient-to-b from-green-700 transition-all overflow-hidden">
       <div
         id="logo"
-        className="w-44 mt-auto p-4 ml-4 shrink-0 md:w-56 lg:w-64 lg:ml-2"
+        className="w-44 p-4 ml-4 shrink-0 md:w-56 lg:w-64 lg:ml-2"
       >
         <a href="/">
           <img src={logo} alt="zamil logo" />
         </a>
       </div>
-      <div className="ml-auto mt-4 mr-6 focus:outline-none">
-        <Hamburger className="text-3xl block lg:hidden focus:ring border rounded-md md:text-5xl p-1 border-green-500" />
+      <div className="ml-auto inline-block content-center">
+        <Hamburger className="mr-6 mt-3 text-3xl lg:hidden md:text-5xl p-1 focus:ring text-white" />
       </div>
-      <nav className="m-auto mr-8 text-white text-xl space-x-7 hidden lg:flex">
-        <ul className="flex space-x-4 text-center navbarAllignment text-white focus:">
-          <li className={liStyle}>
-            <a href="/">Home</a>
-          </li>
-          <li className={liStyle}>
-            <a href="/">About Us</a>
-          </li>
-          <li className={liStyle}>
-            <a href="/">Business Divisions</a>
-          </li>
-          <li className={liStyle}>
-            <a href="/">Our Clients</a>
-          </li>
-          <li className={liStyle}>
-            <a href="/">Careers</a>
-          </li>
-          <li className={liStyle}>
-            <a href="/">العربية</a>
-          </li>
-        </ul>
+      <nav
+        className="m-auto mr-8 text-white text-xl space-x-7 hidden lg:flex bg-black p-4 rounded-md border border-green-400">
+        <LinksList mainStyles={ulStyles} liStyles={liStyles} />
       </nav>
     </header>
   );
