49b7bda52398f0fc0e556d0112cef27e3f17d177
bassam <tenmayos_2@hotmail.com>
2023-09-25 16:50:05 +0300

    removed spring components from Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import Hamburger from "./Hamburger.jsx";
 import LinksList from './LinksList.jsx';
-import { animated, useSpring } from '@react-spring/web';
+import { animated, useTransition } from '@react-spring/web';
 import logo from "../assets/png/logo.png";
 
 
@@ -8,24 +8,14 @@ function NavBar() {
   const ulStyles = 'inline-flex space-x-6 ul-allignment p-4 text-white';
   const liStyles = "transition-all hover:text-green-400";
   const btnLiStyles = 'transition-all bg-green-600 rounded-full hover:bg-black hover:text-green-600';
-  // Remove fade effect. looks bad.
-  const [styleSprings, api] = useSpring(() => {
-    return {
-      from: { opacity: 0 },
-      to: { opacity: 1 },
-      config: { duration: 1000 }
-    }
-  });
 
   // will need a state to know when the hamburger has been clicked
   return (
     <animated.header
-      className="flex border-black bg-gradient-to-b from-green-700 transition-all overflow-hidden"
-      style={styleSprings}
-    >
+      className="flex border-black bg-gradient-to-b from-green-700 transition-all overflow-hidden">
       <div
         id="logo"
-        className="w-44 p-4 ml-4 shrink-0 md:w-56 lg:w-56 lg:ml-8"
+        className="w-44 p-4 ml-4 shrink-0 md:w-56 lg:w-52 lg:mt-2 lg:ml-8"
       >
         <a href="/">
           <img src={logo} alt="zamil logo" />
@@ -39,7 +29,6 @@ function NavBar() {
         <div className="bg-black bg-opacity-50 text-base hidden lg:flex rounded-2xl border border-green-700">
           <LinksList mainStyles={ulStyles} liStyles={liStyles} btnLiStyles={btnLiStyles} />
         </div>
-
       </nav>
     </animated.header>
   );
