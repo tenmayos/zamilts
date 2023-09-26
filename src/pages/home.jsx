756f910a2d34243293fb52f1eedc9328fe7e66c7
bassam <tenmayos_2@hotmail.com>
2023-09-26 16:41:16 +0300

    Added Hero component to home page

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,9 +1,11 @@
 import NavBar from "../components/Navbar"
+import Hero from '../components/Hero'
 
 export default function Home() {
     return (
         <>
             <NavBar />
+            <Hero />
         </>
     )
 }
