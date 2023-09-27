75839e68cb5d1353a03def31e91ae6f02a68eacf
bassam <tenmayos_2@hotmail.com>
2023-09-27 14:43:36 +0300

    added intro section to the index

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,11 +1,13 @@
 import NavBar from "../components/Navbar"
 import Hero from '../components/Hero'
+import Intro from "../components/Intro"
 
 export default function Home() {
     return (
         <>
             <NavBar />
             <Hero />
+            <Intro />
         </>
     )
 }
