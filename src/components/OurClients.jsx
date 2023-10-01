060eac9faff1ca59e400af42debeabd80b70a654
bassam <tenmayos_2@hotmail.com>
2023-10-01 16:26:48 +0300

    reduced the grid's rows and columns inserted a comment to signal carousel location

--- src/components/OurClients.jsx
+++ src/components/OurClients.jsx
@@ -1,7 +1,7 @@
 function OurClients() {
     return (
-        <section className="grid grid-cols-6 grid-rows-4">
-            <div className="text-center col-start-2 col-span-4">
+        <section className="grid grid-cols-2 grid-rows-2 md:grid-rows-2 pb-20">
+            <div className="text-center col-start-1 col-span-2">
                 <h1 className="text-3xl">
                     <strong>
                         Our Clients
@@ -11,6 +11,7 @@ function OurClients() {
                     We are considered as one of the most reputed trading organizations of Saudi Arabia and is the best when it comes to total reliability
                 </p>
             </div>
+            {/* insert the partners and clients carousel in here */}
         </section>
     )
 }
