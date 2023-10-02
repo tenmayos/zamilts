c7d8b760be4c93a6d6f4031e323a7b5d021092c8
bassam <tenmayos_2@hotmail.com>
2023-10-02 15:15:57 +0300

    Added current year

--- src/components/MicroFooter.jsx
+++ src/components/MicroFooter.jsx
@@ -1,9 +1,12 @@
 function MicroFooter() {
+
+    const currentYear = new Date().getFullYear();
+    
     return (
         <div className='text-center'>
             <hr className='border border-gray-200 my-4 w-2/3 m-auto' />
             <span className='text-sm text-center pb-4'>
-                © 2022 Zamil Trade & Services All rights reserved
+                © {currentYear} Zamil Trade & Services All rights reserved
             </span>
         </div>
     )
