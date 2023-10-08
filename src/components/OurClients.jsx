3bd02bd981a7e369b01804b96039d85d9e187afe
bassam <tenmayos_2@hotmail.com>
2023-10-08 16:15:46 +0300

    added preparations for adding the clients carousel

--- src/components/OurClients.jsx
+++ src/components/OurClients.jsx
@@ -1,4 +1,5 @@
 function OurClients() {
+
     return (
         <section className="grid grid-cols-2 grid-rows-2 md:grid-rows-2 pb-10">
             <div className="text-center col-start-1 col-span-2">
@@ -11,7 +12,10 @@ function OurClients() {
                     We are considered as one of the most reputed trading organizations of Saudi Arabia and is the best when it comes to total reliability
                 </p>
             </div>
-            {/* insert the partners and clients carousel in here */}
+            <div className="row-start-2 col-span-2">
+                {/* insert the partners and clients carousel in here */}
+            </div>
+
         </section>
     )
 }
