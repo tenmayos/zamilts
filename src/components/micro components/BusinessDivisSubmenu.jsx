3d1e44abd1e1b2b1b314f18f036ecf57d3e610d7
bassam <tenmayos_2@hotmail.com>
2023-10-08 14:01:29 +0300

    prepared to add submenu integration

--- src/components/micro components/BusinessDivisSubmenu.jsx
+++ src/components/micro components/BusinessDivisSubmenu.jsx
@@ -2,27 +2,35 @@ import { animated } from "@react-spring/web";
 
 function BusinessDivisSubmenu(props) {
 
-    return (
-        <animated.ul style={props.springStyles} className="p-2 text-white">
-            <li className="p-3 border-b border-green-600">
-                Oil and Gas
-            </li>
-            <li className="p-3 border-b border-green-600">
-                Chemicals
-            </li>
-            <li className="p-3 border-b border-green-600">
-                Mechanical
-            </li>
-            <li className="p-3 border-b border-green-600">
-                Construction
-            </li>
-            <li className="p-3 border-b border-green-600">
-                Oil & Gas Division
-            </li>
-            <li className="p-3 border-green-600">
-                Telecoms and Security
-            </li>
-        </animated.ul>
-    )
+    if (props.type === 'pc') {
+        return (
+            null
+        )
+    } else {
+        return (
+            <animated.ul style={props.springStyles} className="p-2 text-white">
+                <li className="p-3 border-b border-green-600">
+                    Oil and Gas
+                </li>
+                <li className="p-3 border-b border-green-600">
+                    Chemicals
+                </li>
+                <li className="p-3 border-b border-green-600">
+                    Mechanical
+                </li>
+                <li className="p-3 border-b border-green-600">
+                    Construction
+                </li>
+                <li className="p-3 border-b border-green-600">
+                    Oil & Gas Division
+                </li>
+                <li className="p-3 border-green-600">
+                    Telecoms and Security
+                </li>
+            </animated.ul>
+        )
+    }
+
+
 }
 export default BusinessDivisSubmenu;
