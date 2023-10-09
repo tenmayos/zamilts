4c82e608426471b087a6b1fae4cccf3462df2bc1
bassam <tenmayos_2@hotmail.com>
2023-10-09 15:04:41 +0300

    fixed original carousel to move by px instead of %

--- src/components/ClientsCarousel.jsx
+++ src/components/ClientsCarousel.jsx
@@ -1,23 +1,21 @@
 import { useEffect, useState } from "react";
 
-function ClientsCarousel({ children: slides, autoSlide = false, autoSlideInterval = 3000, allowedViews = 1 }) {
+function ClientsCarousel({ children: slides, autoSlideInterval = 3000 }) {
     // slide at 50% when inner width is bigger than 769
     const [currentIndex, setCurrentIndex] = useState(0);
 
     function prev() {
         setCurrentIndex(
-            prevState => prevState === 0 ? allowedViews : prevState - 1
+            prevState => prevState === 0 ? slides.length - 1 : prevState - 1
         )
     }
     function next() {
         setCurrentIndex(
-            prevState => prevState === allowedViews ? 0 : prevState + 1
+            prevState => prevState === slides.length - 1 ? 0 : prevState + 1
         )
     }
 
     useEffect(() => {
-        if (!autoSlide)
-            return
         const slideInterval = setInterval(next, autoSlideInterval);
         return () => clearInterval(slideInterval);
     }, [])
@@ -25,7 +23,7 @@ function ClientsCarousel({ children: slides, autoSlide = false, autoSlideInterva
     return (
         <div className="relative">
             <div className="flex transition-transform ease-out duration-500"
-                style={{ transform: `translateX(-${currentIndex * 100}%)` }}>
+                style={{ transform: `translateX(-${currentIndex * 335}px)` }}>
                 {slides}
             </div>
             <div className="absolute inset-0 flex items-center justify-between">
