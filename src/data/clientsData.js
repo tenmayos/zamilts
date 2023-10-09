85d7d6a867e8819802d367980671c744121dd7d3
bassam <tenmayos_2@hotmail.com>
2023-10-09 12:52:56 +0300

    clients pics array exported from clientsData.js

---
+++ src/data/clientsData.js
@@ -0,0 +1,31 @@
+import client1 from '../assets/png/client icons/client_1.png'
+import client2 from '../assets/png/client icons/client_2.png'
+import client3 from '../assets/png/client icons/client_3.png'
+import client4 from '../assets/png/client icons/client_4.png'
+import client5 from '../assets/png/client icons/client_5.png'
+import client6 from '../assets/png/client icons/client_6.png'
+import client7 from '../assets/png/client icons/client_7.png'
+import client8 from '../assets/png/client icons/client_8.png'
+import client9 from '../assets/png/client icons/client_9.png'
+import client10 from '../assets/png/client icons/client_10.png'
+import client11 from '../assets/png/client icons/client_11.png'
+import client12 from '../assets/png/client icons/client_12.png'
+import client13 from '../assets/png/client icons/client_13.png'
+import client14 from '../assets/png/client icons/client_14.png'
+import client15 from '../assets/png/client icons/client_15.png'
+import client16 from '../assets/png/client icons/client_16.png'
+import client17 from '../assets/png/client icons/client_17.png'
+import client18 from '../assets/png/client icons/client_18.png'
+import client19 from '../assets/png/client icons/client_19.png'
+import client20 from '../assets/png/client icons/client_20.png'
+import client21 from '../assets/png/client icons/client_21.png'
+
+
+const fullClientsData = [
+    client1, client2, client3, client4, client5,
+    client6, client7, client8, client9, client10,
+    client11, client12, client13, client14, client15,
+    client16, client17, client18, client19, client20, client21
+]
+
+export default fullClientsData;
