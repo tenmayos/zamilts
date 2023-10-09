2ca6f513e0f6b76955823ccc9581172f279f48cd
bassam <tenmayos_2@hotmail.com>
2023-10-09 12:54:46 +0300

    implemented ClientsCarousel with a temp hackaround TODO: render the carousel based on view width

--- src/components/OurClients.jsx
+++ src/components/OurClients.jsx
@@ -1,8 +1,11 @@
+import ClientsCarousel from "./ClientsCarousel";
+import fullClientsData from "../data/clientsData";
+
 function OurClients() {
 
     return (
-        <section className="grid grid-cols-2 grid-rows-2 md:grid-rows-2 pb-10">
-            <div className="text-center col-start-1 col-span-2">
+        <section className="grid grid-rows-2 content-center m-4">
+            <div className="row-start-1 text-center m-auto">
                 <h1 className="text-3xl">
                     <strong>
                         Our Clients
@@ -12,10 +15,19 @@ function OurClients() {
                     We are considered as one of the most reputed trading organizations of Saudi Arabia and is the best when it comes to total reliability
                 </p>
             </div>
-            <div className="row-start-2 col-span-2">
+            <div className="row-start-2 flex justify-center">
                 {/* insert the partners and clients carousel in here */}
+                <div className="max-w-sm md:max-w-2xl overflow-hidden">
+                    <ClientsCarousel autoSlide={false} allowedViews={(fullClientsData.length - 1) / 2}>
+                        {
+                            fullClientsData.map(
+                                (src) =>
+                                    <img className="resized" src={src} />
+                            )
+                        }
+                    </ClientsCarousel>
+                </div>
             </div>
-
         </section>
     )
 }
