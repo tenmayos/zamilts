f761e8a37e4722d9f9532e448acc1add81409c7e
bassam <tenmayos_2@hotmail.com>
2023-10-09 15:49:06 +0300

    moved hr out of microfooter into footer

--- src/components/MicroFooter.jsx
+++ src/components/MicroFooter.jsx
@@ -4,7 +4,6 @@ function MicroFooter() {
     
     return (
         <div className='text-center'>
-            <hr className='border border-gray-200 my-4 w-2/3 lg:w-10/12 m-auto' />
             <span className='text-sm text-center pb-4'>
                 © {currentYear} Zamil Trade & Services All rights reserved
             </span>
