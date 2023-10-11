08f9e767d659ee1e366891cbdadb157e8272b61e
bassam <tenmayos_2@hotmail.com>
2023-10-11 14:01:21 +0300

    added language support for AboutUsSubmenu

--- src/components/micro components/AboutUsSubmenu.jsx
+++ src/components/micro components/AboutUsSubmenu.jsx
@@ -1,6 +1,8 @@
 import { animated } from '@react-spring/web';
 import visionIcon from '../../assets/png/vision.png';
 import companyIcon from '../../assets/png/company.png';
+import { useLocation } from 'react-router-dom';
+import { aboutUsSubmenuLanguages } from '../../data/navbarData';
 
 /**
  * 
@@ -9,6 +11,8 @@ import companyIcon from '../../assets/png/company.png';
  */
 function AboutUsSubmenu(props) {
 
+    const languagePack = useLocation().pathname.includes('/ar') ? aboutUsSubmenuLanguages.ar : aboutUsSubmenuLanguages.en;
+
     if (props.type === 'pc') {
         return (
             <animated.li style={props.springs} onMouseLeave={props.exitFunc} className='absolute top-full bg-white rounded-md border border-green-600'>
@@ -16,13 +20,13 @@ function AboutUsSubmenu(props) {
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-12 mx-auto' src={companyIcon} alt="" />
-                            <span className='block'>Company Profile</span>
+                            <span className='block'>{languagePack[0]}</span>
                         </div>
                     </li>
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-12 mx-auto' src={visionIcon} alt="" />
-                            <span className='block'>Vision and Mission</span>
+                            <span className='block'>{languagePack[1]}</span>
                         </div>
                     </li>
                 </ul>
@@ -33,10 +37,10 @@ function AboutUsSubmenu(props) {
         return (
             <animated.ul style={props.springStyles} className="p-2 text-white">
                 <li className="p-3 border-b border-green-600">
-                    Company Profile
+                    {languagePack[0]}
                 </li>
                 <li className="p-3 border-green-600">
-                    Our Vision
+                    {languagePack[1]}
                 </li>
             </animated.ul>
         )
