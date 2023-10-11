bb06d8571aa3d94b0c67aa764e9ebd62b22bc0fa
bassam <tenmayos_2@hotmail.com>
2023-10-11 13:59:20 +0300

    added explicit routing for language support

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home/home'
+import NotFound from './pages/NotFound/notFound'
 // ONE IMPORT TO RULE THEM ALL
 import './styles/main.css'
 
@@ -8,7 +9,10 @@ function App() {
   return (
     <>
       <Routes>
-        <Route index path='/' element={<Home />}/>
+        <Route index path='/' element={<Home />} />
+        <Route index path='/ar' element={<Home />} />
+
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   )
