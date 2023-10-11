5fabd2d9d1f6f30c525325d2300974f54e3c0af6
bassam <tenmayos_2@hotmail.com>
2023-10-11 16:38:17 +0300

    added language data for the homepage

---
+++ src/pages/Home/PageData/homeData.js
@@ -0,0 +1,59 @@
+const heroLanguages = {
+    en: [
+        'Welcome to',
+        'Zamil',
+        'Trade & ',
+        'Services',
+        'Zamil Group Trade & Services was formed in the mid 1930s. The enterprising, foresighted and innovative spirit which...',
+    ],
+    ar: [
+        "مرحبا بكم في مجموعة",
+        "الزامل",
+        " للتجارة و",
+        "الخدمات",
+        "تأسست مجموعة الزامل للتجارة والخدمات في منتصف الثلاثينيات...",
+    ],
+}
+
+export { heroLanguages };
+
+const introLanguages = {
+    en: [
+        'Zamil Group Trade & Services',
+        'Zamil Group Trade & Services was formed in the mid 1930s. The enterprising, foresighted and innovative spirit which saw the vast and varied market potential in the kingdom has not only survived; it has blossomed, creating one of Saudi Arabia’s largest and most diversified trading organizations.',
+        'We function as partners to overseas manufacturers and we are proud to represent many well-known international companies, trading establishments, services and consultancy firms. Our vast experience and enthusiastic team of experts has made us the friendliest and most welcomed supplier to our esteemed clients...',
+        'Read More'
+    ],
+    ar: [
+        'مجموعة الزامل للتجارة والخدمات',
+        'تأسست مجموعة الزامل للتجارة والخدمات في منتصف الثلاثينيات. إن روح المغامرة والبصيرة والابتكار التي شهدت إمكانات السوق الواسعة والمتنوعة في المملكة لم تنج فقط ؛ لقد ازدهرت ، وأوجدت واحدة من أكبر المنظمات التجارية وأكثرها تنوعًا في المملكة العربية السعودية.',
+        'نحن نعمل كشركاء للمصنعين في الخارج ونفخر بتمثيل العديد من الشركات العالمية والمؤسسات التجارية والخدمات والشركات الاستشارية المعروفة. لقد جعلتنا خبرتنا الواسعة وفريق الخبراء المتحمس منا المورد الأكثر ودية والأكثر ترحيبًا لعملائنا الكرام ...',
+        'اقراء المزيد'
+    ]
+}
+
+export { introLanguages };
+
+const servicesLanguage = {
+    en: [
+        'Value added Services'
+    ],
+    ar: [
+        'خدمات ذات قيمة إضافية'
+    ]
+}
+
+export { servicesLanguage };
+
+const clientsLanguage = {
+    en: [
+        'Our Clients',
+        'We are considered as one of the most reputed trading organizations of Saudi Arabia and is the best when it comes to total reliability'
+    ],
+    ar: [
+        'عملائنا',
+        'نحن نعتبر كواحدة من أكثر المؤسسات التجارية شهرة في المملكة العربية السعودية وهي الأفضل عندما يتعلق الأمر '
+    ]
+}
+
+export { clientsLanguage };
