26dca7143bb37e9c21dcad8da4ef0b968f9bd13b
bassam <tenmayos_2@hotmail.com>
2023-10-11 16:39:41 +0300

    added language pack to the intro section

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,8 +1,11 @@
 import aboutPic from '../assets/png/about.png'
 import PrimaryButton from './PrimaryButton';
+import { useLocation } from 'react-router-dom';
+import { introLanguages } from '../pages/Home/PageData/homeData';
 
 function Intro() {
-
+    const isArabic = useLocation().pathname.includes('/ar');
+    const languagesPack = isArabic ? introLanguages.ar : introLanguages.en;
     return (
         <section className="md:grid md:grid-cols-2 text-left my-12 p-4">
             <div className='flex md:justify-center md:self-center'>
@@ -11,21 +14,17 @@ function Intro() {
             <div className='flex flex-wrap justify-start self-center mt-4 py-1 lg:pr-14'>
                 <h1 className='text-left text-2xl'>
                     <strong>
-                        Zamil Group Trade & Services
+                        {languagesPack[0]}
                     </strong>
                 </h1>
                 <p className="text-base text-left mt-4 leading-relaxed">
-                    Zamil Group Trade & Services was formed in the mid 1930s.
-                    The enterprising,
-                    foresighted and innovative spirit which saw the vast and varied market potential in the kingdom has not only survived; it has blossomed, creating one of Saudi Arabia’s largest and most diversified trading organizations.
+                    {languagesPack[1]}
                 </p>
                 <p className="text-base mt-4 text-left leading-relaxed">
-                    We function as partners to overseas manufacturers and we are proud to represent many well-known international companies,
-                    trading establishments, services and consultancy firms.
-                    Our vast experience and enthusiastic team of experts has made us the friendliest and most welcomed supplier to our esteemed clients...
+                    {languagesPack[2]}
                 </p>
                 <div className="mt-6">
-                    <PrimaryButton innerText='Read More' />
+                    <PrimaryButton innerText={languagesPack[3]} />
                 </div>
             </div>
         </section>
