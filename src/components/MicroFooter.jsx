77349723e4a643ab2b4d891a19431cbb5f4b25dd
bassam <tenmayos_2@hotmail.com>
2023-10-11 16:39:54 +0300

    added language pack to the MicroFooter

--- src/components/MicroFooter.jsx
+++ src/components/MicroFooter.jsx
@@ -1,11 +1,16 @@
+import { useLocation } from "react-router-dom";
+import { microfooterLanguages } from "../data/footerData";
+
 function MicroFooter() {
 
     const currentYear = new Date().getFullYear();
-    
+    const isArabic = useLocation().pathname.includes('/ar');
+    const languagePack = isArabic ? microfooterLanguages.ar : microfooterLanguages.en;
+
     return (
         <div className='text-center'>
             <span className='text-sm text-center pb-4'>
-                © {currentYear} Zamil Trade & Services All rights reserved
+                © {currentYear + ` ${languagePack[0]}`}
             </span>
         </div>
     )
