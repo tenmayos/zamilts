4c4f292ffe4133291ee558debbb1f12b6209ab31
bassam <tenmayos_2@hotmail.com>
2023-10-11 14:01:57 +0300

    added language support for BusinessDivisSubmenu

--- src/components/micro components/BusinessDivisSubmenu.jsx
+++ src/components/micro components/BusinessDivisSubmenu.jsx
@@ -4,9 +4,11 @@ import gasIcon from '../../assets/png/department icons/gas.png';
 import construcIcon from '../../assets/png/department icons/construction.png';
 import mechIcon from '../../assets/png/department icons/mechanical.png';
 import oilIcon from '../../assets/png/department icons/oil.png';
-
+import { businessDivisSubmenuLanguages } from '../../data/navbarData';
+import { useLocation } from 'react-router-dom';
 
 function BusinessDivisSubmenu(props) {
+    const languagePack = useLocation().pathname.includes('/ar') ? businessDivisSubmenuLanguages.ar : businessDivisSubmenuLanguages.en;
 
     if (props.type === 'pc') {
         return (
@@ -17,37 +19,37 @@ function BusinessDivisSubmenu(props) {
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-10 mx-auto' src={oilIcon} alt="" />
-                            <span className='block text-sm'>Oil & Gas Department</span>
+                            <span className='block text-sm'>{languagePack[0]}</span>
                         </div>
                     </li>
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-10 mx-auto' src={chemIcon} alt="" />
-                            <span className='block text-sm'>Chemical Department</span>
+                            <span className='block text-sm'>{languagePack[1]}</span>
                         </div>
                     </li>
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-10 mx-auto' src={mechIcon} alt="" />
-                            <span className='block text-sm'>Mechanical Department</span>
+                            <span className='block text-sm'>{languagePack[2]}</span>
                         </div>
                     </li>
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-10 mx-auto' src={construcIcon} alt="" />
-                            <span className='block text-sm'>Construction Department</span>
+                            <span className='block text-sm'>{languagePack[3]}</span>
                         </div>
                     </li>
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-10 mx-auto' src={gasIcon} alt="" />
-                            <span className='block text-sm'>Oil & Gas Division</span>
+                            <span className='block text-sm'>{languagePack[4]}</span>
                         </div>
                     </li>
                     <li className='transition-colors text-center p-2 hover:text-green-600 cursor-pointer'>
                         <div className='relative text-center'>
                             <img className='w-10 mx-auto' src={gasIcon} alt="" />
-                            <span className='block text-sm'>Telecomms & Security</span>
+                            <span className='block text-sm'>{languagePack[5]}</span>
                         </div>
                     </li>
                 </ul>
@@ -57,22 +59,22 @@ function BusinessDivisSubmenu(props) {
         return (
             <animated.ul style={props.springStyles} className="p-2 text-white">
                 <li className="p-3 border-b border-green-600">
-                    Oil and Gas
+                    {languagePack[0]}
                 </li>
                 <li className="p-3 border-b border-green-600">
-                    Chemicals
+                    {languagePack[1]}
                 </li>
                 <li className="p-3 border-b border-green-600">
-                    Mechanical
+                    {languagePack[2]}
                 </li>
                 <li className="p-3 border-b border-green-600">
-                    Construction
+                    {languagePack[3]}
                 </li>
                 <li className="p-3 border-b border-green-600">
-                    Oil & Gas Division
+                    {languagePack[4]}
                 </li>
                 <li className="p-3 border-green-600">
-                    Telecoms and Security
+                    {languagePack[5]}
                 </li>
             </animated.ul>
         )
