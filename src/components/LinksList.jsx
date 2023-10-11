bb80ec71a1a934224d0366f51dbd0fb3e04248b8
bassam <tenmayos_2@hotmail.com>
2023-10-11 14:00:48 +0300

    added language support for linksList

--- src/components/LinksList.jsx
+++ src/components/LinksList.jsx
@@ -1,8 +1,11 @@
 import { animated, useSpring } from '@react-spring/web';
 import { useState } from 'react';
 import { useTransition } from '@react-spring/web';
+import { useLocation } from 'react-router-dom';
+import { linksListLanguages } from '../data/navbarData';
 import AboutUsSubmenu from './micro components/AboutUsSubmenu';
 import BusinessDivisSubmenu from './micro components/BusinessDivisSubmenu';
+import { Link } from 'react-router-dom';
 /**
  * @param {string} mainStyles ul's CSS classes
  * @param {string} liStyles li's CSS Classes
@@ -14,6 +17,8 @@ function LinksList(props) {
   const [getInHovered, setGetInHover] = useState(false);
   const [aboutUsHovered, setAboutUsHovered] = useState(false);
   const [bdivisHovered, setBdivisHovered] = useState(false);
+  const isArabic = useLocation().pathname.includes('/ar');
+  const languagePack = isArabic ? linksListLanguages.ar : linksListLanguages.en;
 
   const aboutSubmenuTransition = useTransition(aboutUsHovered, {
     from: { opacity: 0 },
@@ -42,13 +47,13 @@ function LinksList(props) {
     <>
       <ul className={props.mainStyles}>
         <li className={props.liStyles}>
-          <a href="/">Home</a>
+          <a href="/">{languagePack[0]}</a>
         </li>
         <li className={props.liStyles}
           onMouseEnter={() => {
             setAboutUsHovered(true);
           }}>
-          <a href="/">About Us</a>
+          <a href="/">{languagePack[1]}</a>
         </li>
         {
           aboutSubmenuTransition(
@@ -65,7 +70,7 @@ function LinksList(props) {
           onMouseEnter={() => {
             setBdivisHovered(true);
           }}>
-          <a href="/">Business Divisions</a>
+          <a href="/">{languagePack[2]}</a>
         </li>
         {
           bDivisSubmenuTransition(
@@ -80,13 +85,13 @@ function LinksList(props) {
           )
         }
         <li className={props.liStyles}>
-          <a href="/">Our Clients</a>
+          <a href="/">{languagePack[3]}</a>
         </li>
         <li className={props.liStyles}>
-          <a href="/">Careers</a>
+          <a href="/">{languagePack[4]}</a>
         </li>
         <li className={props.liStyles}>
-          <a href="/">العربية</a>
+          {isArabic ? <Link to="/">{languagePack[5]}</Link> : <Link to='/ar'>{languagePack[5]}</Link>}
         </li>
         <li className='flex-auto bg-green700'>
           <animated.a
@@ -101,7 +106,7 @@ function LinksList(props) {
             {
               getInHovered &&
               <div className='inline-block pr-2 pl-1'>
-                Get in Touch
+                {languagePack[6]}
               </div>
             }
             <i className="fa-solid fa-phone-volume"></i>
