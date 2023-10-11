f978be383e2383ad289ac8f07fb18a58d5fe83ae
bassam <tenmayos_2@hotmail.com>
2023-10-11 14:00:28 +0300

    relocated homepage location

---
+++ src/pages/Home/home.jsx
@@ -0,0 +1,24 @@
+import NavBar from "../../components/Navbar"
+import Hero from '../../components/Hero'
+import Intro from "../../components/Intro"
+import Services from "../../components/Services"
+import OurClients from "../../components/OurClients"
+import Footer from "../../components/Footer"
+import GetIn from "../../components/GetIn"
+
+function Home() {
+
+    return (
+        <>
+            <NavBar />
+            <Hero />
+            <Intro />
+            <Services />
+            <OurClients />
+            {/* <GetIn /> */}
+            <Footer />
+        </>
+    )
+}
+
+export default Home;
